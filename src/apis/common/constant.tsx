bcac0ff2190cf1519848904ca3176d8283bb6570
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-11-15 00:52:15 +0900

    add: API Instance & API url

---
+++ src/apis/common/constant.tsx
@@ -0,0 +1,15 @@
+export const PATH = {
+  USERS_: "/users",
+  // 전체 유저 리스트 조회
+  USERS_List: "/list",
+  // 로그인
+  USERS_LOGIN: "/login",
+  // 로그인 - 리프레시 토큰
+  USERS_LOGIN_REFRESH: "/refresh",
+  // 로그아웃
+  USERS_LOGOUT: "/logout",
+  // 회원가입
+  USERS_SIGNUP: "/signup",
+  // 유저 프로필
+  USERS_PROFILE: "/profile",
+};
