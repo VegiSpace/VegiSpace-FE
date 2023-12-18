2de351de9231c990c87bc951604f77d2df24d42c
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-12-18 18:05:29 +0900

    add: Recipe Card component:

--- src/pages/Recipe/RecipeCard/index.tsx
+++ src/pages/Recipe/RecipeCard/index.tsx
@@ -1,8 +1,11 @@
-import { RecipeThumbImg } from "assets";
+import { ClockBlack, ClockWhite, RecipeThumbImg } from "assets";
+import { Typo } from "components";
+import { borderRadius, colors } from "styles/options";
 import styled from "styled-components";
 
 const Recipes = [
   {
+    key: 0,
     title: "레시피",
     food: "고구마, 양배추 & 가지",
     detail:
@@ -11,6 +14,7 @@ const Recipes = [
     srcImg: RecipeThumbImg,
   },
   {
+    key: 1,
     title: "레시피",
     food: "고구마, 양배추 & 가지",
     detail:
@@ -19,6 +23,7 @@ const Recipes = [
     srcImg: RecipeThumbImg,
   },
   {
+    key: 2,
     title: "레시피",
     food: "고구마, 양배추 & 가지",
     detail:
@@ -27,6 +32,7 @@ const Recipes = [
     srcImg: RecipeThumbImg,
   },
   {
+    key: 3,
     title: "레시피",
     food: "고구마, 양배추 & 가지",
     detail:
@@ -35,6 +41,7 @@ const Recipes = [
     srcImg: RecipeThumbImg,
   },
   {
+    key: 4,
     title: "레시피",
     food: "고구마, 양배추 & 가지",
     detail:
@@ -47,16 +54,22 @@ const Recipes = [
 const RecipeCard = () => {
   return (
     <div>
-      {Recipes.map(({ title, food, detail, time, srcImg }, idx) => (
-        <CardContainer key={idx}>
+      {Recipes.map(({ title, food, detail, time, srcImg, key }) => (
+        <CardContainer key={key} isGreen={Boolean((key + 1) % 2)}>
           <img src={srcImg} alt={title} />
           <InfoContainer>
-            <p>
-              {title}#{idx + 1}
-            </p>
-            <p>{food}</p>
-            <p>{detail}</p>
-            <p>{time}</p>
+            <Typo typoType="SubTitle2">
+              {title}#{key + 1}
+            </Typo>
+            <Typo typoType="Body4">{food}</Typo>
+            <Typo typoType="Body4">{detail}</Typo>
+            <TimeWrapper isGreen={Boolean((key + 1) % 2)}>
+              <img
+                src={(key + 1) % 2 ? ClockWhite : ClockBlack}
+                alt={`clock img ${key % 2}`}
+              />
+              <Typo typoType="Caption1">{time}</Typo>
+            </TimeWrapper>
           </InfoContainer>
         </CardContainer>
       ))}
@@ -66,11 +79,16 @@ const RecipeCard = () => {
 
 export default RecipeCard;
 
-const CardContainer = styled.div`
+const CardContainer = styled.div<{ isGreen: boolean }>`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 0.8rem 0;
+  padding: 0.8rem;
+  background-color: ${(props) =>
+    props.isGreen ? colors.neutral[0] : colors.primary[300]};
+  border-radius: ${borderRadius.normal};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  margin: 1.4rem 0;
 `;
 
 const InfoContainer = styled.div`
@@ -79,4 +97,18 @@ const InfoContainer = styled.div`
   align-items: flex-start;
   justify-content: space-around;
   padding-left: 1.6rem;
+`;
+
+const TimeWrapper = styled.div<{ isGreen: boolean }>`
+  width: 8.5rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${(props) =>
+    props.isGreen ? colors.primary[300] : colors.neutral[0]};
+  padding: 0.4rem 0.8rem;
+  border-radius: ${borderRadius.square};
+  color: ${(props) =>
+    props.isGreen ? colors.neutral[0] : colors.neutral[1000]};
 `;
